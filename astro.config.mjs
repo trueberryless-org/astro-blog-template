d49b76f6b532d7b4994a4f5aa6c53372301c6e14
trueberryless <trueberryless+github@gmail.com>
2025-04-18 23:33:24 +0200

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+import svelte from '@astrojs/svelte';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://example.com',
+    integrations: [mdx(), sitemap(), svelte()],
+});
