14d0ade051419b6c1930ffc79ed48c826b261d7a
trueberryless <trueberryless+github@gmail.com>
2025-04-19 10:54:29 +0200

    feat: add robots.txt file for builds

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 import { defineConfig } from "astro/config";
 
+import robotsTxt from "astro-robots-txt";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [mdx(), sitemap(), svelte()],
+  site: "https://astro-blog-template-trueberryless.netlify.app",
+  integrations: [mdx(), sitemap(), svelte(), robotsTxt()],
 });
